afaa694d910d0a5376d1ec1fb0e692584f8c0eab
James N <jnjangi@worcester.edu>
2019-03-12 16:37:23 -0400

    .

---
+++ Pythagorean Triangle Checker.py
@@ -0,0 +1,25 @@
+# A program that allows the user to input the sides of any triangle, and then
+# return whether the triangle is a Pythagorean Triple or not
+
+play = input("Do you want to proceed? Y/N: ")
+
+while play != 'Y' or play == 'y':
+    a = input("Please the opposite value: ")
+    print("you entered " + a)
+
+    b = input("Please the adjacent value: ")
+    print("you entered " + b)
+
+    c = input("Please the hypotenuse value: ")
+    print("you entered " + c)
+
+    if int(a) ** 2 + int(b) ** 2 == int(c) ** 2:
+        print("The triangle is a pythagorean triangle")
+    else:
+        print("The triangle is not a pythagorean triangle")
+
+
+
+
+
+
