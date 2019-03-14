e5a6a7aa5a7fd993e5a3724603af10d60937e745
James N <jnjangi@worcester.edu>
2019-03-13 22:56:58 -0400

    -Edited to account for entering numbers or any other character instead of Y/N -Shortened code to remove redundancy

--- Pythagorean Triangle Checker.py
+++ Pythagorean Triangle Checker.py
@@ -1,25 +1,25 @@
 # A program that allows the user to input the sides of any triangle, and then
 # return whether the triangle is a Pythagorean Triple or not
 
-play = input("Do you want to proceed? Y/N: ")
+while True:
+    question = input("Do you want to continue? Y/N: ")
+    if question.upper() != 'Y' and question.upper() != 'N':
+        print("Please type Y or N")
+    elif question.upper() == 'Y':
+        a = input("Please enter the opposite value: ")
+        print("you entered " + a)
 
-while play != 'Y' or play == 'y':
-    a = input("Please enter the opposite value: ")
-    print("you entered " + a)
+        b = input("Please enter the adjacent value: ")
+        print("you entered " + b)
 
-    b = input("Please enter the adjacent value: ")
-    print("you entered " + b)
+        c = input("Please enter the hypotenuse value: ")
+        print("you entered " + c)
 
-    c = input("Please enter the hypotenuse value: ")
-    print("you entered " + c)
-
-    if int(a) ** 2 + int(b) ** 2 == int(c) ** 2:
-        print("The triangle is a pythagorean triangle")
+        if int(a) ** 2 + int(b) ** 2 == int(c) ** 2:
+            print("The triangle is a pythagorean triangle")
+        else:
+            print("The triangle is not a pythagorean triangle")
     else:
-        print("The triangle is not a pythagorean triangle")
-
-
-
-
+        break
 
 
